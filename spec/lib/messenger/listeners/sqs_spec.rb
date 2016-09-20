16babe7a0ecc3a6e7820a41d731695418dde6262
Lucas Amorim <lucasamorim@protonmail.com>
2016-09-20 15:17:15 -0700

    Adds specs

--- spec/lib/messenger/listeners/sqs_spec.rb
+++ spec/lib/messenger/listeners/sqs_spec.rb
@@ -55,7 +55,39 @@ describe Messenger::Listeners::Sqs do
         expect { listener.listen }.to raise_error
       end
     end
-
   end
 
+  context '#submit_message' do
+    let(:listener) { Messenger::Listeners::Sqs.new }
+    let(:worker) { stub(work: :message) }
+    let(:message) { double(receipt_handle: 'something') }
+    let(:client) { double }
+
+    before { listener.instance_variable_set(:@worker, worker) }
+    before { Messenger::Listeners::Sqs.configure { |config| config.queue_url = QUEUE_URL } }
+    before { listener.instance_variable_set(:@sqs, client) }
+    before { expect(client).to receive(:delete_message).and_raise(error) }
+    before { expect(worker).to_not receive(:work) }
+
+    context 'AWS::SQS::Errors::InvalidParameterValue exception' do
+      let(:error) { AWS::SQS::Errors::InvalidParameterValue }
+      it 'should not raise an error' do
+        expect(listener.send(:submit_message, message)).to be_nil
+      end
+    end
+
+    context 'AWS::SQS::Errors::InvalidParameterValue exception' do
+      let(:error) { AWS::SQS::Errors::RequestExpired }
+      it 'should not raise an error' do
+        expect(listener.send(:submit_message, message)).to be_nil
+      end
+    end
+
+    context 'AWS::SQS::Errors::InvalidParameterValue exception' do
+      let(:error) { AWS::SQS::Errors::ServiceUnavailable }
+      it 'should not raise an error' do
+        expect(listener.send(:submit_message, message)).to be_nil
+      end
+    end
+  end
 end
